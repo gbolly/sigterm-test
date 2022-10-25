d3f2a71aa708a2fea5d2d5028c8d9b860fb87246
Gbolahan Okerayi <okerayigbolahan@gmail.com>
2022-10-25 15:07:04 +0100

    Update sig.py

--- sig.py
+++ sig.py
@@ -36,11 +36,11 @@ if sys.argv[1] == "handle_signal":
     signal.signal(signal.SIGTERM, sigterm_handler)
 
 try:
-    print "Hello"
+    print("Hello")
     i = 0
     while True:
         i += 1
-        print "Iteration #%i" % i
+        print("Iteration #%i" % i)
         sleep(1)
 finally:
-    print "Goodbye"
+    print("Goodbye")
