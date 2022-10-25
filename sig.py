46f37e1084e9367b7419c72e9ef788155e986f06
Gbolahan Okerayi <okerayigbolahan@gmail.com>
2022-10-25 13:54:01 +0100

    Update sig.py

--- sig.py
+++ sig.py
@@ -7,14 +7,14 @@ logger = logging.getLogger(__name__)
 
 def sigterm_handler(_signo, _stack_frame):
     print("SIGTEM DETECTED!!!!!")
-    logger.warning('Script terminated...uploading log files')
-    logger.info("Uploading log files")
+    logging.warning('Script terminated...uploading log files')
+    logging.info("Uploading log files")
 
 signal.signal(signal.SIGTERM, sigterm_handler)
 
 
 def main():
-    logger.info("Running script to test job cancellation on platform")
+    print("Running script to test job cancellation on platform")
     time.sleep(300)
 
 if __name__ == "__main__":
