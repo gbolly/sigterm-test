3908c71540db5545f2ae1f5de6ec529f1a8bef1c
Gbolahan Okerayi <okerayigbolahan@gmail.com>
2022-10-25 14:25:44 +0100

    Update sig.py

--- sig.py
+++ sig.py
@@ -14,5 +14,6 @@ def sigterm_handler(_signo, _stack_frame):
 if __name__ == "__main__":
     signal.signal(signal.SIGTERM, sigterm_handler)
     print("Starting script")
-    time.sleep(120)
+    for i in range(0, 3000):
+        print(i)
     print("Running script to test job cancellation on platform")
